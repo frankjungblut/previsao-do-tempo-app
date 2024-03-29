d25580a1138e8e5499e446ff0e6afe43845d122f
Frank Jungblut <franksjungblut@gmail.com>
2024-03-29 00:53:17 -0300

    recurso para selecionar unidade de medida celsius ou farenheit

--- script.js
+++ script.js
@@ -1,22 +1,37 @@
 const key = "8635dd4261ddadaa1e2f8abcdf1cbe37"
-
+let unit = `metric`
+let letter = " °C"
 
 function displayData(data) {
     console.log(data)
     document.querySelector(".city").innerHTML = "Weather in " + data.name
-    document.querySelector(".temperature").innerHTML = Math.floor(data.main.temp) + " °C"
+    document.querySelector(".temperature").innerHTML = Math.floor(data.main.temp) + letter
     document.querySelector(".prediction-text").innerHTML = data.weather[0].description
     document.querySelector(".humidity").innerHTML = "Humidity: " + data.main.humidity + "%"
-    document.querySelector(".prediction-img").src=`https://openweathermap.org/img/wn/${data.weather[0].icon}.png`
+    document.querySelector(".prediction-img").src = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`
 }
 
 async function searchCity(city) {
-    const data = await fetch(` https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&lang=pt_br&units=metric`).then( response => response.json())
+    const data = await fetch(` https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=${unit}`).then(response => response.json())
     displayData(data)
 }
 
 function buttonActive() {
     const city = document.querySelector(".city-input").value
     searchCity(city)
+}
 
+function toggleUnit(params) {
+    if (params === 'celsius') {
+        console.log("celsius")
+        unit = `metric`
+        letter = " °C"
+        buttonActive()
+    }
+    if (params === 'fahrenheit') {
+        console.log('fahrenheit')
+        unit = ``
+        letter = " °F"
+        buttonActive()
+    }
 }
